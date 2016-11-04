db334cdb060e0bd1382a3b030e8d517fb503a523
Warner Nielsen <email": null>
2016-11-04 16:06:40 -0700

    Add tests for admin controller, WIP, add index to movie controller and a view index for movies

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe AdminController, type: :controller do
 
+  describe "GET #index" do
+    xit "responds with status code 200" do
+      get :index
+      expect(response).to have_http_status 200
+    end
+  end
+
 end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
 
+
+
 end
