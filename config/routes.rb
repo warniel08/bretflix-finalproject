3530c2f5f8bd1a0c0b2f88806cf316e06ef20047
Katherine Guenther <guenther@peak.org>
2016-11-04 14:30:48 -0700

    Create routes nested under admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
+
+  get '/login', to: 'login'
+  get '/logout', to: 'logout'
+  get '/register', to: 'register'
+  get '/movies', to: 'movies'
+
+
+  resources :admin
+  resources :movies
+  resources :users
+
+  scope 'admin' do
+    resources :movies
+    resources :users
+  end
+
 end
