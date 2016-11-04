8730638dd1c13f9056daf28167b812316597ee77
Warner Nielsen <email": null>
2016-11-04 11:10:21 -0700

    Movie create not working, user create is working

--- db/schema.rb
+++ db/schema.rb
@@ -16,21 +16,25 @@ ActiveRecord::Schema.define(version: 20161104163621) do
   enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
-    t.string  "title"
-    t.integer "year"
-    t.string  "description"
-    t.boolean "checked_out"
-    t.integer "checkout_count"
-    t.date    "due_date"
-    t.integer "user_id"
-    t.integer "condition"
+    t.string   "title"
+    t.integer  "year"
+    t.string   "description"
+    t.boolean  "checked_out"
+    t.integer  "checkout_count"
+    t.date     "due_date"
+    t.integer  "user_id"
+    t.integer  "condition"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "password"
-    t.string "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
