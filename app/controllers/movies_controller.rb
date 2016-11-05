52d9ef211f79ba515f2c03ba08a4fb190d765f14
Warner Nielsen <email": null>
2016-11-04 17:36:55 -0700

    Add pagination to movies index opage

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.order(:title).page params[:page]
+    @movies = Movie.all.order(:title)
+    @movies = Kaminari.paginate_array(@movies).page(params[:page]).per(10)
   end
 
   def show
