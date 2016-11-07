c6a188efee2cf488ea194298a6e41b2d28302a3d
Katherine Guenther <guenther@peak.org>
2016-11-06 18:10:36 -0800

    Generate uri for keyword search

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,2 +1,35 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$( document ).ready(function() {
+
+// Submits on enter
+  $("#keyword").on('keyup', function(e){
+    if (e.which == 13) {
+      $("#keyword-search").submit();
+    }
+  });  
+
+
+  $("#keyword-search").on('submit', function(e){
+    e.preventDefault();
+    var $form = $(e.target)
+    var keyword = $form.serializeArray()[0].value;
+    console.log(keyword)
+    var searchURL = 'movies/search/'+ keyword
+    console.log(searchURL)
+    // var promise = $.ajax({
+    //   url: searchURL,
+    //   method: 'GET',
+    //   datatype: "JSON"
+    // });
+
+    // promise.done(function(arrayOfTweets){
+    //   $("#tweets-container ul li").addClass("hidden")
+    //   processTweets(arrayOfTweets);
+    //   $("#search").removeClass("fail-search");
+    // });
+
+    // promise.error(function(){
+    //   $("#search").addClass("fail-search");
+    // })
+
+  });
+});
