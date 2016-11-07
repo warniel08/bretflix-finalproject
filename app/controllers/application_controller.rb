46b029bed060c68da8c73e27c3d7215b9776e204
Warner Nielsen <email": null>
2016-11-07 12:49:28 -0800

    Take turbolinks out of gemfile, fix strong params on app controller to handle first and last name when registering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-    # protected
+    protected
 
-    #   def configure_permitted_parameters
-    #     devise_parameter_sanitizer.for(:new) { |u| u.permit(:first_name, :last_name, :email, :password) }
-    #     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password) }
-    #   end
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password) }
+      end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to '/movies'
   end
+
+  private
+    def user_params
+      params.required(:user).permit(:first_name, :last_name, :password, :email)
+    end
 end
+
+# Couple of questions, why does it keep giving me the errors about first and last name being blank????????????
+# Why, when the errors appear does the route change to /users? and not stay on the registration page?????????????
