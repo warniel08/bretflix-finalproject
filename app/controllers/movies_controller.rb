72c295220667add517adb4345e645af122c2adfb
apprentice <apprentice@apprentice-48.local>
2016-11-08 11:29:12 -0800

    Add update method to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.user = current_user
+    @movie.checkout_count +=1
+    @movie.due_date = get_due_date
+    @movie.save!
+  end
+
+  private
+
+  def get_due_date
+    Date.today + 7
+  end
 end
