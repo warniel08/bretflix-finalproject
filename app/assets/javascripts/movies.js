37fce6ee2c2236fa785d8d32ac07444e7cf06e24
Katherine Guenther <guenther@peak.org>
2016-11-07 17:13:37 -0800

    Display list of matches by keyword

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,19 +1,21 @@
-function processMovies(arrayOfMovies){
-  var movieList = "<h1>Search Results by Keyword</h1>";
-
-  for (var i = 0; i < arrayOfMovies.length; i++) {
-    movieList +="<li>"+arrayOfMovies[i].title+"</li>";
-  };
-  $("#main-content").replaceWith(movieList);
-};
-
-
 $( document ).ready(function() {
+
+  function processMovies(arrayOfMovies){
+    var movieList = "<h1>Search Results by Keyword</h1>";
+
+    for (var i = 0; i < arrayOfMovies.length; i++) {
+      movieList +="<li>"+arrayOfMovies[i].title+"</li>";
+    };
+    $("#main-content").append(movieList);
+    $('.hide-on-keyword-search').hide();
+  };
+
 
 // Submits on enter
   $("#keyword").on('keyup', function(e){
     if (e.which == 13) {
       $("#keyword-search").submit();
+      return false;
     }
   });
 
@@ -22,6 +24,7 @@ $( document ).ready(function() {
     e.preventDefault();
     var $form = $(e.target)
     var keyword = $form.serializeArray()[0].value;
+    debugger;
     var searchURL = 'movies/search/'+keyword
     var promise = $.ajax({
       url: searchURL,
