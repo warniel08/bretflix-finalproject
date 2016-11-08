99eec7fb8a62a15c82019614a2bc9cf0c39ad22b
Warner Nielsen <email": null>
2016-11-07 17:36:42 -0800

    Update migration file to rename password to encrypted_password

--- db/migrate/20161107184806_add_devise_to_users.rb
+++ db/migrate/20161107184806_add_devise_to_users.rb
@@ -5,6 +5,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
 
       t.string :encrypted_password, null: false, default: ""
 
+      t.rename :password, :encrypted_password
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -32,7 +34,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
 
 
       # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
