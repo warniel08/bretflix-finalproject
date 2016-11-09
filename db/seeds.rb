1c8aade0a814484b733cf50587ae204091a03185
Warner Nielsen <email": null>
2016-11-09 12:04:26 -0800

    Fix seed data file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,11 @@
+require 'csv'
+filename = 'db/Collection.csv'
 users = []
 
 10.times do
   users << User.create!({:first_name => Faker::Name.first_name, :last_name => Faker::Name.last_name, :email => Faker::Internet.email, :password => "warner"})
 end
 
-10.times do
-  Movie.create!({:title => Faker::Book.title, year: 2016, description: "Awesome", checkout_count: 4, due_date: Faker::Date.forward(2), condition: 2})
-end
 
-10.times do
-  Movie.create!({:title => Faker::Book.title, year: 2016, description: "Awesome", checkout_count: 4, due_date: Faker::Date.forward(2), condition: 2, user: users.first })
-end
+Movie.generate_movie_data(filename)
+
