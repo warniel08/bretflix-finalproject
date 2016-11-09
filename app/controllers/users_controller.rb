d82102100650923caed7c0f73f93d47a96ebb51c
Warner Nielsen <email": null>
2016-11-09 11:50:19 -0800

    Delete spec helper files, change user controller file, add devise user contorller files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new(user_params)
-
-    @user.save
-    redirect_to 'profile_path'
   end
 
   def update
@@ -15,7 +11,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.required(:user).permit(:first_name, :last_name, :password, :email)
+      params.required(:user).permit(:first_name, :last_name, :email)
     end
 end
 
