d48a68519fc51dd233b69307eea674a58921d98a
Warner Nielsen <email": null>
2016-11-09 14:02:18 -0800

    Update gemfile, fix migration file to what it was originally

--- db/migrate/20161104163612_create_users.rb
+++ db/migrate/20161104163612_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :encrypted_password
+      t.string :encrypted_password, null: false
       t.string :email
 
       t.timestamps null: false
