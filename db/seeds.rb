36f5a9459c769806a561210305facb99db3f2893
Warner Nielsen <bwn888@gmail.com>
2016-11-10 15:40:20 -0800

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,11 @@ users = []
 # Generate admin
 User.create!({:first_name => 'Bret', :last_name => 'Bassett', :email => ENV["GMAIL_USERNAME"], :password => ENV["ADMIN_PASSWORD"]})
 
-10.times do
+3.times do
   users << User.create!({:first_name => Faker::Name.first_name, :last_name => Faker::Name.last_name, :email => Faker::Internet.email, :password => "warner"})
 end
+
+users << User.create!({:first_name => "Mary", :last_name => "Jones", :email => "mary@mary.com", :password => "marymary"})
 
 
 Movie.generate_movie_data(filename)
