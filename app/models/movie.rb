9c6515a85cb1fabc90d54f88c82e3d2e4452e45a
Warner Nielsen <email": null>
2016-11-09 16:59:29 -0800

    Update movie model to pull description data from csv file

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,16 +10,17 @@ class Movie < ApplicationRecord
   def self.generate_movie_data(filename)
     title_data = []
     year_data = []
+    tt_data = []
     CSV.foreach(filename) do |row|
       title_data << row[2]
       year_data << row[5]
+      tt_data << row[6]
     end
 
     i = 0
     while i < title_data.length do
-      Movie.find_or_create_by(title: title_data[i], year: year_data[i], checkout_count: 0)
+      Movie.find_or_create_by(title: title_data[i], year: year_data[i], description: tt_data[i], checkout_count: 0)
       i +=1
-
     end
   end
 end
