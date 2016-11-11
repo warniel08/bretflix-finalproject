bdaccb071d0c61443b66a92c8e60d233c8cb272e
Warner Nielsen <bwn888@gmail.com>
2016-11-10 16:20:50 -0800

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@ filename = 'db/Collection.csv'
 users = []
 
 # Generate admin
-User.create!({:first_name => 'Bret', :last_name => 'Bassett', :email => ENV["GMAIL_USERNAME"], :password => ENV["ADMIN_PASSWORD"]})
+User.find_or_create_by({:first_name => 'Bret', :last_name => 'Bassett', :email => ENV["GMAIL_USERNAME"], :password => ENV["ADMIN_PASSWORD"]})
 
-users << User.create!({:first_name => "Mary", :last_name => "Jones", :email => "mary@mary.com", :password => "marymary"})
+users << User.find_or_create_by({:first_name => "Mary", :last_name => "Jones", :email => "mary@mary.com", :password => "marymary"})
 
-users << User.create!({:first_name => "Harry", :last_name => "Potter", :email => "harry@hogwarts.com", :password => "123456"})
+users << User.find_or_create_by({:first_name => "Harry", :last_name => "Potter", :email => "harry@hogwarts.com", :password => "123456"})
 
 Movie.generate_movie_data(filename)
 
